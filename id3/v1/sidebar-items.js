b0456207f02282c229326880ae1d2e17a5e212b4
Travis CI User <travis@example.org>
2018-06-05 20:09:11 +0000

    Documentation for jameshurst/rust-id3@ff92c9b

---
+++ id3/v1/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Tag","A structure containing ID3v1 metadata."]]});
---
+++ implementors/core/str/trait.FromStr.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["derive_builder_core"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"struct\" href=\"derive_builder_core/struct.Block.html\" title=\"struct derive_builder_core::Block\">Block</a>",synthetic:false,types:["derive_builder_core::block::Block"]},];
+implementors["id3"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"struct\" href=\"id3/struct.Timestamp.html\" title=\"struct id3::Timestamp\">Timestamp</a>",synthetic:false,types:["id3::frame::timestamp::Timestamp"]},];
+implementors["regex"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"struct\" href=\"regex/bytes/struct.Regex.html\" title=\"struct regex::bytes::Regex\">Regex</a>",synthetic:false,types:["regex::re_bytes::Regex"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"struct\" href=\"regex/struct.Regex.html\" title=\"struct regex::Regex\">Regex</a>",synthetic:false,types:["regex::re_unicode::Regex"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
